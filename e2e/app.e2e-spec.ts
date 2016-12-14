8ac0af2ccce8b5581b92c162df6e50b1654f47cf
angular-cli <angular-cli@angular.io>
2016-12-14 14:08:47 +0900

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Angular2StartHandsonPage } from './app.po';
+
+describe('angular2-start-handson App', function() {
+  let page: Angular2StartHandsonPage;
+
+  beforeEach(() => {
+    page = new Angular2StartHandsonPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
