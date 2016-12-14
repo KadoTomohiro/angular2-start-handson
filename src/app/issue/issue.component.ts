29ee23d3409557074dfcf42cc56d816022b8ffb1
KadoTomohiro <corner0427@gmail.com>
2016-12-14 16:28:06 +0900

    issue formの実装

--- src/app/issue/issue.component.ts
+++ src/app/issue/issue.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Issue} from "./issue";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-issue',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssueComponent implements OnInit {
 
+  private issue: Issue;
+  //noinspection JSMismatchedCollectionQueryUpdate
+  private issues: Issue[];
+
   constructor() { }
 
-  ngOnInit() {
+  //noinspection JSUnusedGlobalSymbols
+  ngOnInit(): void {
+    this.issue = new Issue;
+    this.issues = [];
+  }
+
+  public onSubmit(form: NgForm): void {
+    const issue = {
+      title: form.value.title,
+      desc: form.value.desc
+    };
+
+    this.issues.push(issue);
+
+    form.reset();
+  }
+
+  public onDelete(index: number): void {
+    this.issues.splice(index, 1);
   }
 
 }
